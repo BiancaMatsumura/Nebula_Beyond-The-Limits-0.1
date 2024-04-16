d8c5dc7bc393433bb08d188e2b6a8cce450c1b1b
Bianca Dellevedove Matsumura <bdmatsumura@sga.pucminas.br>
2024-04-15 22:33:54 -0300

    tentativa de colocar a tela de Game Over (falta fazer funcionar)

--- Nebula - Beyond The Limits/Assets/Scripts/bulletController.cs
+++ Nebula - Beyond The Limits/Assets/Scripts/bulletController.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class bulletController : MonoBehaviour
 {
     public float speed = 10f;
+    public int dano = 10;
 
     void Update()
     {
 
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+            collision.gameObject.GetComponent<vidaJogador>().TakeDamage(dano);
+
+            
+            Destroy(gameObject);
+        }
+    }
 }
+
---
+++ Nebula - Beyond The Limits/Assets/Scripts/gameOverScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameOverScreen : MonoBehaviour
+{
+
+    public void RestartButton() {
+        SceneManager.LoadScene("Menu");
+    
+    }
+    
+
+}
